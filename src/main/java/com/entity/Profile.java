9f5e11be044ede4831dc798a4bbd8f89ee8a3165
Danil <81990805+SPuDI-zzz@users.noreply.github.com>
2023-05-12 22:06:31 +0300

    remove constructor

--- src/main/java/com/entity/Profile.java
+++ src/main/java/com/entity/Profile.java
@@ -9,12 +9,4 @@ public class Profile {
     private long id;
     private String login;
     private String password;
-
-    public Profile(long id, String login, String password) {
-        this.id = id;
-        this.login = login;
-        this.password = password;
-    }
-
-
 }
