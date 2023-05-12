81094f9924c4dffc64ce37a3214b76f945025c77
Danil <81990805+SPuDI-zzz@users.noreply.github.com>
2023-05-12 22:03:58 +0300

    add DTOs

---
+++ src/main/java/com/dto/ProfileDTO.java
@@ -0,0 +1,11 @@
+package com.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class ProfileDTO {
+    private Long id;
+    private String login;
+}
