3f1c8624704a8fae2e8cd637961b62aeedbd5df0
Danil <81990805+SPuDI-zzz@users.noreply.github.com>
2023-05-12 22:05:39 +0300

    add lombok in Profile and Vehicle

--- src/main/java/com/entity/Profile.java
+++ src/main/java/com/entity/Profile.java
@@ -1,7 +1,10 @@
 package com.entity;
 
-import java.util.Objects;
+import lombok.Builder;
+import lombok.Data;
 
+@Builder
+@Data
 public class Profile {
     private long id;
     private String login;
@@ -13,50 +16,5 @@ public class Profile {
         this.password = password;
     }
 
-    public long getId() {
-        return id;
-    }
 
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Profile profile = (Profile) o;
-        return id == profile.id && Objects.equals(login, profile.login)
-                && Objects.equals(password, profile.password);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, login, password);
-    }
-
-    @Override
-    public String toString() {
-        return "Profile{" +
-                "id=" + id +
-                ", login='" + login + '\'' +
-                ", password='" + password + '\'' +
-                '}';
-    }
 }
